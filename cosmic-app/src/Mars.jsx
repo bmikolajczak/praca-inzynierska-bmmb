42194b4a5d45f5e90f46b2c6fa6059c12ac6e43e
M4thez <namemat@gmail.com>
2022-08-08 16:49:48 +0200

    Overview page early prototype

--- cosmic-app/src/Mars.jsx
+++ cosmic-app/src/Mars.jsx
@@ -1,19 +1,34 @@
 import './Mars.css'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Html } from '@react-three/drei'
+import React, {useState, useRef} from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+
+function Planet() {
+  const ref = useRef()
+  useFrame(() => (ref.current.rotation.y += 0.005))
+  return (
+    <mesh ref={ref} scale={4} position={[-3, 0, -3]}>
+      <sphereGeometry />
+      <meshStandardMaterial color="red"/>
+    </mesh>
+  )
+}
 
 function Mars() {
   return (
-    <div id="canvas-container">
-      <Canvas >
-        <ambientLight intensity={0.1}/>
-        <directionalLight color="blue" position={[0,0,5]}/>
-        <OrbitControls/>
-        <mesh>
-          <octahedronGeometry/>
-          <meshStandardMaterial color="white"/>
-        </mesh> 
-      </Canvas>
+    <div id="overview-container">
+      <div id="canvas-container">
+        <Canvas >
+          <ambientLight intensity={0.2} />
+          <pointLight color="white" position={[0, 0, 5]} />
+          <Planet/>
+        </Canvas>
+      </div>
+      <div id="overview-info">
+        <h1>Title Mars</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ab totam incidunt nam dicta deleniti sapiente cupiditate, illum iure voluptatibus beatae aperiam. Dignissimos, vero blanditiis similique eius deserunt eaque officia qui doloremque officiis suscipit.
+        </p>
+      </div>
     </div>
   )
 }
