80781363e9f412d079f36af59cc82e8b73ece880
bmikolajczak <br.mikolajczak@gmail.com>
2022-08-31 01:09:49 +0200

    testing css/scss modules

--- cosmic-app/src/pages/Mars/components/Mars.jsx
+++ cosmic-app/src/pages/Mars/components/Mars.jsx
@@ -1,32 +1,31 @@
-import '../styles/Mars.css'
-import React, { Suspense, useRef } from 'react'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import "../styles/Mars.css";
+import React, { Suspense, useRef } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
+import { paragraph } from "../styles/Mars.module.scss";
 // Example mesh
 function Planet() {
-  const ref = useRef()
-  useFrame(() => (ref.current.rotation.y += 0.005))
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y += 0.005));
   return (
     <mesh ref={ref} scale={4} position={[-3, 0, -3]}>
       <sphereGeometry />
       <meshStandardMaterial color="red" />
     </mesh>
-  )
+  );
 }
 
 // Imported 3D Globe model
 function MarsGlobe() {
-  const gltf = useLoader(GLTFLoader, '/src/assets/MarsGlobe2.glb')
-  const ref = useRef()
-  useFrame(() => (
-    ref.current.rotation.y += 0.0001
-  ))
+  const gltf = useLoader(GLTFLoader, "/src/assets/MarsGlobe2.glb");
+  const ref = useRef();
+  useFrame(() => (ref.current.rotation.y += 0.0001));
   return (
     <Suspense fallback={null}>
       <primitive object={gltf.scene} ref={ref} scale={4} position={[-2, 0, -1]} />
     </Suspense>
-  )
+  );
 }
 
 // Canvas
@@ -43,11 +42,22 @@ function Mars() {
       </div>
       <div id="overview-info">
         <h1>Mars - the Red Planet</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi aspernatu.<br/><br/> Nihil eos nemo esse officia, mollitia ipsum quam ex quae dolorem quos amet? Tempora sunt modi soluta reiciendis dolores voluptatibus, distinctio ad magnam quaerat necessitatibus fugit odit corporis reprehenderit!
+        <p className="paragraph">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi aspernatu.
+          <br />
+          <br /> Nihil eos nemo esse officia, mollitia ipsum quam ex quae dolorem quos amet? Tempora sunt modi soluta
+          reiciendis dolores voluptatibus, distinctio ad magnam quaerat necessitatibus fugit odit corporis
+          reprehenderit!
+        </p>
+        <p className={paragraph}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi aspernatu.
+          <br />
+          <br /> Nihil eos nemo esse officia, mollitia ipsum quam ex quae dolorem quos amet? Tempora sunt modi soluta
+          reiciendis dolores voluptatibus, distinctio ad magnam quaerat necessitatibus fugit odit corporis
+          reprehenderit!
         </p>
       </div>
     </div>
-  )
+  );
 }
-export default Mars
+export default Mars;
