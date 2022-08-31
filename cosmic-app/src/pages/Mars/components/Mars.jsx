c4d238d9860e05b608c0240b814d795a462693b4
Mateusz Boboryko <58702250+M4thez@users.noreply.github.com>
2022-08-31 13:40:46 +0200

    Css modules (#3)
    * testing css/scss modules
    
    * Added sass dependency
    
    * Added prettier
    
    * Prettier run
    
    * Added new line
    
    Co-authored-by: bmikolajczak <br.mikolajczak@gmail.com>

--- cosmic-app/src/pages/Mars/components/Mars.jsx
+++ cosmic-app/src/pages/Mars/components/Mars.jsx
@@ -3,6 +3,7 @@ import React, { Suspense, useRef } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
+import { paragraph } from '../styles/Mars.module.scss'
 // Example mesh
 function Planet() {
   const ref = useRef()
@@ -19,12 +20,15 @@ function Planet() {
 function MarsGlobe() {
   const gltf = useLoader(GLTFLoader, '/src/assets/MarsGlobe2.glb')
   const ref = useRef()
-  useFrame(() => (
-    ref.current.rotation.y += 0.0001
-  ))
+  useFrame(() => (ref.current.rotation.y += 0.0001))
   return (
     <Suspense fallback={null}>
-      <primitive object={gltf.scene} ref={ref} scale={4} position={[-2, 0, -1]} />
+      <primitive
+        object={gltf.scene}
+        ref={ref}
+        scale={4}
+        position={[-2, 0, -1]}
+      />
     </Suspense>
   )
 }
@@ -43,8 +47,23 @@ function Mars() {
       </div>
       <div id="overview-info">
         <h1>Mars - the Red Planet</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi aspernatu.<br/><br/> Nihil eos nemo esse officia, mollitia ipsum quam ex quae dolorem quos amet? Tempora sunt modi soluta reiciendis dolores voluptatibus, distinctio ad magnam quaerat necessitatibus fugit odit corporis reprehenderit!
+        <p className="paragraph">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi
+          aspernatu.
+          <br />
+          <br /> Nihil eos nemo esse officia, mollitia ipsum quam ex quae
+          dolorem quos amet? Tempora sunt modi soluta reiciendis dolores
+          voluptatibus, distinctio ad magnam quaerat necessitatibus fugit odit
+          corporis reprehenderit!
+        </p>
+        <p className={paragraph}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi
+          aspernatu.
+          <br />
+          <br /> Nihil eos nemo esse officia, mollitia ipsum quam ex quae
+          dolorem quos amet? Tempora sunt modi soluta reiciendis dolores
+          voluptatibus, distinctio ad magnam quaerat necessitatibus fugit odit
+          corporis reprehenderit!
         </p>
       </div>
     </div>
