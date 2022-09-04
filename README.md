34dffa30a083421e3972a8d975d42023c4603bf5
M4thez <namemat@gmail.com>
2022-09-04 20:13:29 +0200

    Cleaned up styles

--- README.md
+++ README.md
@@ -1,46 +1,61 @@
 # praca-inzynierska-bmmb
 
 ## Development
+
 Figma designs
+
 - https://www.figma.com/files/team/1048369725744733499
 
 ### Useful links
+
 React Three Fiber
+
 - https://docs.pmnd.rs/
 - https://docs.pmnd.rs/react-three-fiber
 - https://docs.pmnd.rs/react-three-fiber/api/objects
 
 Useful helpers
+
 - https://github.com/pmndrs/react-postprocessing#readme
 - https://github.com/pmndrs/drei#readme
 
 GLTF assets to JSX components
+
 - https://github.com/pmndrs/gltfjsx
 - https://gltf.pmnd.rs/
 
 React animations
+
 - https://react-spring.dev/
 
 ### Examples
+
 - https://codesandbox.io/s/mixing-html-and-webgl-7ucso?file=/src/App.js
 - https://codesandbox.io/s/flexboxyoga-in-webgl-7psew?file=/src/App.js
 
 ### Inspiration
+
 Mars
+
 - https://mars.nasa.gov/
 - https://mars.nasa.gov/all-about-mars/facts/
 - https://www.space.com/41254-touring-mars-red-planet-road-trip.html
 
 ## Commands
+
 Setup
+
 ```
 npm install
 ```
 
 Start dev server
+
 ```
 npm run dev
 ```
 
 ## Resources
+
 - https://www.solarsystemscope.com/textures/ - solar system planets textures and background images
+- https://www.svgrepo.com/svg/53673/planet - saturn svg icon
--- cosmic-app/src/pages/Mars/components/Mars.jsx
+++ cosmic-app/src/pages/Mars/components/Mars.jsx
@@ -1,24 +1,12 @@
-import '../styles/Mars.css'
 import React, { Suspense, useRef } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-
-import { paragraph } from '../styles/Mars.module.scss'
-// Example mesh
-function Planet() {
-  const ref = useRef()
-  useFrame(() => (ref.current.rotation.y += 0.005))
-  return (
-    <mesh ref={ref} scale={4} position={[-3, 0, -3]}>
-      <sphereGeometry />
-      <meshStandardMaterial color="red" />
-    </mesh>
-  )
-}
+import style from '../styles/Mars.module.scss'
+import '../styles/Mars.scss'
 
 // Imported 3D Globe model
 function MarsGlobe() {
-  const gltf = useLoader(GLTFLoader, '/src/assets/MarsGlobe2.glb')
+  const gltf = useLoader(GLTFLoader, '/src/assets/MarsGlobe.glb')
   const ref = useRef()
   useFrame(() => (ref.current.rotation.y += 0.0001))
   return (
@@ -36,27 +24,17 @@ function MarsGlobe() {
 // Canvas
 function Mars() {
   return (
-    <div id="overview-container">
-      <div id="canvas-container">
+    <div className={style.overviewContainer}>
+      <div className={style.canvasContainer}>
         <Canvas camera={{ fov: 90 }}>
           <ambientLight intensity={0.2} />
           <pointLight color="white" position={[0, 0, 5]} />
-          {/* <Planet/> */}
           <MarsGlobe />
         </Canvas>
       </div>
-      <div id="overview-info">
+      <div className={style.overviewInfo}>
         <h1>Mars - the Red Planet</h1>
-        <p className="paragraph">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi
-          aspernatu.
-          <br />
-          <br /> Nihil eos nemo esse officia, mollitia ipsum quam ex quae
-          dolorem quos amet? Tempora sunt modi soluta reiciendis dolores
-          voluptatibus, distinctio ad magnam quaerat necessitatibus fugit odit
-          corporis reprehenderit!
-        </p>
-        <p className={paragraph}>
+        <p className={style.paragraph}>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque quasi
           aspernatu.
           <br />
