c9b3081dd0a916aac73125c7ed2ede2bf370566b
M4thez <namemat@gmail.com>
2022-09-05 14:14:53 +0200

    Added loading screen

--- cosmic-app/src/pages/Mars/components/Mars.jsx
+++ cosmic-app/src/pages/Mars/components/Mars.jsx
@@ -24,31 +24,33 @@ function MarsGlobe() {
 // Canvas
 function Mars() {
   return (
-    <div className={style.overviewContainer}>
-      <div className={style.canvasContainer}>
-        <Canvas camera={{ fov: 45 }}>
-          <ambientLight intensity={0.2} />
-          <pointLight color="white" position={[0, 0, 5]} />
-          <MarsGlobe />
-        </Canvas>
+    <Suspense fallback={<span className={style.loading}>Loading...</span>}>
+      <div className={style.overviewContainer}>
+        <div className={style.canvasContainer}>
+          <Canvas camera={{ fov: 45 }}>
+            <ambientLight intensity={0.2} />
+            <pointLight color="white" position={[0, 0, 5]} />
+            <MarsGlobe />
+          </Canvas>
+        </div>
+        <div className={style.overviewInfo}>
+          <h1 className={style.heading}>Mars - the Red Planet</h1>
+          <p>
+            The fourth planet from the Sun, Mars is one of Earth's two closest
+            planetary neighbors.
+            <br />
+            <br /> A dusty, cold, desert world with a very thin atmosphere. Mars
+            is also a dynamic planet with seasons, polar ice caps, canyons,
+            extinct volcanoes, and evidence that it was even more active in the
+            past.
+            <br />
+            <br /> Mars is one of the most explored bodies in our solar system,
+            and it's the only planet where we've sent rovers to roam the alien
+            landscape.
+          </p>
+        </div>
       </div>
-      <div className={style.overviewInfo}>
-        <h1 className={style.heading}>Mars - the Red Planet</h1>
-        <p>
-          The fourth planet from the Sun, Mars is one of Earth's two closest
-          planetary neighbors.
-          <br />
-          <br /> A dusty, cold, desert world with a very thin atmosphere. Mars
-          is also a dynamic planet with seasons, polar ice caps, canyons,
-          extinct volcanoes, and evidence that it was even more active in the
-          past.
-          <br />
-          <br /> Mars is one of the most explored bodies in our solar system,
-          and it's the only planet where we've sent rovers to roam the alien
-          landscape.
-        </p>
-      </div>
-    </div>
+    </Suspense>
   )
 }
 export default Mars
