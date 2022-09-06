c546298b8573f2b671e2925707f816ba87afe274
M4thez <namemat@gmail.com>
2022-09-06 11:17:18 +0200

    Updated apod

--- cosmic-app/src/pages/APOD/components/Apod.jsx
+++ cosmic-app/src/pages/APOD/components/Apod.jsx
@@ -1,19 +1,19 @@
 // APOD - Astronomy Picture of the Day
-import '../styles/Apod.css'
+import '../styles/Apod.scss'
 import React from 'react'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 const API_URL = 'https://api.nasa.gov/planetary/apod?api_key='
 const API_KEY = '0381f1py7G8yhbs9VvrxN9JPn2O5LJ88EEqolGND' // Not secure
 
 function CallApodApi() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [image, setImage] = useState('');
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [image, setImage] = useState('')
 
   useEffect(() => {
     fetch(API_URL + API_KEY)
-      .then(res => res.json())
+      .then((res) => res.json())
       .then(
         (result) => {
           setIsLoaded(true)
@@ -27,28 +27,33 @@ function CallApodApi() {
       )
   }, [])
 
-  if(error) {
+  if (error) {
     return <div>Error: {error.message}</div>
   } else if (!isLoaded) {
     return <div>Loading...</div>
   } else {
     return (
-      <div className='apod-image-container'>
-        <p>{image.title}</p>
-        <img className='apod-image' src={image.url} alt={image.title}/>
-        <i>{image.copyright}</i>
+      <div className="apod-content-container">
+        <p>
+          <b>{image.title}</b>
+        </p>
+        <div className="apod-image-container">
+          <img className="apod-image" src={image.url} alt={image.title} />
+          <div className="apod-info">
+            <p>{image.explanation}</p>
+            <i>@{image.copyright}</i>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-function Apod() {
+export function Apod() {
   return (
-    <div className='apod-container'>
-      <h1>Astronomy Picture of the Day</h1>
-      <CallApodApi/>
+    <div className="apod-container">
+      <h2>Astronomy Picture of the Day</h2>
+      <CallApodApi />
     </div>
-
   )
 }
-export default Apod
