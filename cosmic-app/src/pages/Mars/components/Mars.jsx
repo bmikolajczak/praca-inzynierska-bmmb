d20e14002b0a215467b6bffd0ac547ae92bd4d3c
M4thez <namemat@gmail.com>
2022-09-13 13:58:57 +0200

    Tidied up files

--- cosmic-app/src/pages/Mars/components/Mars.jsx
+++ cosmic-app/src/pages/Mars/components/Mars.jsx
@@ -1,4 +1,3 @@
-import '../styles/Mars.scss'
 import style from '../styles/Mars.module.scss'
 import React, { Suspense, useRef } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
@@ -6,9 +5,9 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 // Imported 3D Globe model
 function MarsGlobe() {
-  const gltf = useLoader(GLTFLoader, '/src/assets/MarsGlobe.glb')
+  const gltf = useLoader(GLTFLoader, '/src/assets/mars/MarsGlobe.glb')
   const ref = useRef()
-  useFrame(() => (ref.current.rotation.y += 0.0001))
+  useFrame(() => (ref.current.rotation.y += 0.0002))
   return (
     <Suspense fallback={null}>
       <primitive
@@ -24,29 +23,31 @@ function MarsGlobe() {
 // Canvas
 function Mars() {
   return (
-    <Suspense fallback={<span className={style.loading}>Loading...</span>}>
-      <div className={style.overviewContainer}>
-        <div className={style.canvasContainer}>
-          <Canvas camera={{ fov: 45 }}>
-            <ambientLight intensity={0.2} />
-            <pointLight color="white" position={[0, 0, 5]} />
-            <MarsGlobe />
-          </Canvas>
+    <main className={style.main}>
+      <Suspense fallback={<span className={style.loading}>Loading...</span>}>
+        <div className={style.overviewContainer}>
+          <div className={style.canvasContainer}>
+            <Canvas camera={{ fov: 45 }}>
+              <ambientLight intensity={0.2} />
+              <pointLight color="white" position={[0, 0, 5]} />
+              <MarsGlobe />
+            </Canvas>
+          </div>
+          <div className={style.overviewInfo}>
+            <h1 className={style.heading}>Mars - the Red Planet</h1>
+            <p>
+              The fourth planet from the Sun, Mars is one of Earth's two closest
+              planetary neighbors.
+              <br />
+              <br /> A dusty, cold, desert world with a very thin atmosphere.
+              Mars is also a dynamic planet with seasons, polar ice caps,
+              canyons, extinct volcanoes, and evidence that it was even more
+              active in the past.
+            </p>
+          </div>
         </div>
-        <div className={style.overviewInfo}>
-          <h1 className={style.heading}>Mars - the Red Planet</h1>
-          <p>
-            The fourth planet from the Sun, Mars is one of Earth's two closest
-            planetary neighbors.
-            <br />
-            <br /> A dusty, cold, desert world with a very thin atmosphere. Mars
-            is also a dynamic planet with seasons, polar ice caps, canyons,
-            extinct volcanoes, and evidence that it was even more active in the
-            past.
-          </p>
-        </div>
-      </div>
-    </Suspense>
+      </Suspense>
+    </main>
   )
 }
 export default Mars
