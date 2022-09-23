43ddf817e09dcbd3525bb40972dba193c7b928c6
M4thez <namemat@gmail.com>
2022-09-23 16:08:37 +0200

    Created new component - Solar

---
+++ cosmic-app/src/pages/Solar System/components/Solar.jsx
@@ -0,0 +1,12 @@
+import style from '../styles/Solar.module.scss'
+import React, { Suspense, useRef } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+export default function Solar() {
+  return (
+    <>
+      <i>HI SOLAR!</i>
+    </>
+  )
+}
