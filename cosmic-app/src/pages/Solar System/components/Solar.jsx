a31b4c87012e7107733929ba652b2532dd82fdbc
M4thez <namemat@gmail.com>
2022-09-23 16:38:14 +0200

    Fixed models position, new exports

--- cosmic-app/src/pages/Solar System/components/Solar.jsx
+++ cosmic-app/src/pages/Solar System/components/Solar.jsx
@@ -3,10 +3,25 @@ import React, { Suspense, useRef } from 'react'
 import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
+function CelestialModel(props) {
+  const modelURL = `src/assets/solar_system/${props.model}`
+  const gltf = useLoader(GLTFLoader, modelURL)
+  const ref = useRef()
+  useFrame(() => (ref.current.rotation.y += 0.0005))
+  return (
+    <Suspense fallback={null}>
+      <primitive object={gltf.scene} ref={ref} scale={1} position={[0, 0, 0]} />
+    </Suspense>
+  )
+}
+
 export default function Solar() {
   return (
-    <>
-      <i>HI SOLAR!</i>
-    </>
+    <main className={style.solar}>
+      <Canvas>
+        <CelestialModel model="earth.glb" />
+        <directionalLight color="white" position={[5, 5, 5]} intensity={1} />
+      </Canvas>
+    </main>
   )
 }
