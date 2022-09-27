9520568ecf0d9e4b39a07b79ee1b4448a149deb9
M4thez <namemat@gmail.com>
2022-09-27 15:39:44 +0200

    All celestials displayed

--- cosmic-app/src/pages/Solar System/components/Solar.jsx
+++ cosmic-app/src/pages/Solar System/components/Solar.jsx
@@ -1,12 +1,17 @@
 import style from '../styles/Solar.module.scss'
 import React, { Suspense, useRef } from 'react'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import celestials from './Solar.json'
+import { OrbitControls } from '@react-three/drei'
 
 function CelestialModel(props) {
   const modelURL = `src/assets/solar_system/${props.model}`
   const gltf = useLoader(GLTFLoader, modelURL)
   const ref = useRef()
+  useThree(({ camera }) => {
+    camera.position.set(0, 60, 125)
+  })
   useFrame(() => (ref.current.rotation.y += 0.0005))
   return (
     <Suspense fallback={null}>
@@ -14,18 +19,31 @@ function CelestialModel(props) {
         object={gltf.scene}
         ref={ref}
         scale={1}
-        position={[0, 5, -20]}
+        position={props.position}
       />
     </Suspense>
   )
 }
 
 export default function Solar() {
+  const celestialBodies = celestials.map((celes) => (
+    <CelestialModel
+      model={celes.model}
+      position={celes.position}
+      key={celes.model}
+    />
+  ))
   return (
     <main className={style.solar}>
-      <Canvas>
-        <CelestialModel model="saturn.glb" />
+      <Canvas camera={{ far: 2500 }}>
+        {celestialBodies}
         <directionalLight color="white" position={[5, 5, 5]} intensity={1} />
+        <OrbitControls
+          makeDefault
+          enableZoom={true}
+          enablePan={true}
+          zoomSpeed={1}
+        />
       </Canvas>
     </main>
   )
