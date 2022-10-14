6eee67c516c77da71241199e5a634aeee815c527
M4thez <namemat@gmail.com>
2022-10-14 13:24:35 +0200

    OrbitRings!!

--- cosmic-app/src/pages/Solar System/components/Solar.jsx
+++ cosmic-app/src/pages/Solar System/components/Solar.jsx
@@ -3,10 +3,16 @@ import React, { Suspense, useRef } from 'react'
 import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import celestials from './Solar.json'
-import { OrbitControls, Environment } from '@react-three/drei'
+import {
+  OrbitControls,
+  Environment,
+  GizmoHelper,
+  GizmoViewport,
+} from '@react-three/drei'
 import Loader from '../../../infrastructure/loader/Loader'
 import Sun from './SunShader'
 import { EffectComposer, Noise, Vignette } from '@react-three/postprocessing'
+import { DoubleSide, MathUtils } from 'three'
 
 function CelestialModel(props) {
   const modelURL = `src/assets/solar_system/${props.model}`
@@ -17,13 +23,15 @@ function CelestialModel(props) {
   // Weird, maybe separate component for camera?
   // If used outside canvas component: Error: R3F hooks can only be used within the Canvas Component
   useThree(({ camera }) => {
-    camera.position.set(0, 60, 125)
+    camera.position.set(0, 30, 125)
   })
-  
-  useFrame(() => (
-    mesh.current.rotation.y += props.spinSpeed,
-    group.current.rotation.y += props.orbitalSpeed * props.orbitalFactor
-  ))
+
+  useFrame(
+    () => (
+      (mesh.current.rotation.y += props.spinSpeed),
+      (group.current.rotation.y += props.orbitalSpeed * props.orbitalFactor)
+    )
+  )
   return (
     <group ref={group}>
       <Suspense fallback={null}>
@@ -37,9 +45,22 @@ function CelestialModel(props) {
     </group>
   )
 }
+function OrbitRing(props) {
+  return (
+    <mesh rotation={[MathUtils.degToRad(90), 0, 0]} position={[0, 0, 0]}>
+      <ringBufferGeometry args={[props.innerRadius, props.outerRadius, 180]} />
+      <meshBasicMaterial color="white" side={DoubleSide} transparent={true} opacity={0.2} />
+    </mesh>
+  )
+}
 
 export default function Solar() {
-  const celestialBodies = celestials.map((celes) => (
+  const celestialBodies = celestials.map((celes) => [
+    <OrbitRing
+      key={'Orbit' + celes.name}
+      innerRadius={celes.position[0] - 0.05}
+      outerRadius={celes.position[0] + 0.05}
+    />,
     <CelestialModel
       model={celes.model}
       position={celes.position}
@@ -47,17 +68,32 @@ export default function Solar() {
       spinSpeed={celes.spinSpeed}
       orbitalSpeed={celes.orbitalSpeed}
       orbitalFactor={0.5}
-    />
-  ))
+    />,
+  ])
   return (
     <main className={style.solar}>
       <Canvas camera={{ far: 2000 }}>
         <Suspense fallback={<Loader />}>
           <Sun />
           {celestialBodies}
-          <pointLight color={'white'} intensity={0.8} position={[0, 0, 0]} decay={2} />
-          <pointLight color={'white'} intensity={0.5} position={[0, 25, 0]} decay={2} />
-          <pointLight color={'white'} intensity={0.5} position={[0, -25, 0]} decay={2} />
+          <pointLight
+            color={'white'}
+            intensity={0.8}
+            position={[0, 0, 0]}
+            decay={2}
+          />
+          <pointLight
+            color={'white'}
+            intensity={0.5}
+            position={[0, 25, 0]}
+            decay={2}
+          />
+          <pointLight
+            color={'white'}
+            intensity={0.5}
+            position={[0, -25, 0]}
+            decay={2}
+          />
           <OrbitControls
             makeDefault
             enableZoom={true}
@@ -76,6 +112,17 @@ export default function Solar() {
           <Noise opacity={0.03} />
           <Vignette eskil={false} offset={0.1} darkness={1.1} />
         </EffectComposer>
+        {/* For dev, axis snap and reference */}
+        {/* <GizmoHelper
+            alignment="bottom-right" // widget alignment within scene
+            margin={[80, 80]} // widget margins (X, Y)
+            renderPriority={1}
+          >
+            <GizmoViewport
+              axisColors={['red', 'green', 'blue']}
+              labelColor="black"
+            />
+          </GizmoHelper> */}
       </Canvas>
     </main>
   )
