b44eda6df7175b8658281dab7ea312703f3a41fb
bmikolajczak <br.mikolajczak@gmail.com>
2022-11-21 15:00:25 +0100

    link grid + mobile grid

--- cosmic-app/src/pages/LinkHub/components/LinkHub.jsx
+++ cosmic-app/src/pages/LinkHub/components/LinkHub.jsx
@@ -8,10 +8,21 @@ export function LinkHub() {
   return (
     <div>
       <div className={styles['links-container']}>
-        <Link to="/mars">Learn about Mars</Link>
-        <Link to="/solar">Learn about our Solar System</Link>
-        <Link to="/apod">See interesting astronomy pictures</Link>
-        <Link to="/about">Learn more about the project</Link>
+        <Link to="/mars" className="mars-li">
+          Learn about Mars
+        </Link>
+        <Link to="/solar" className="solar-li">
+          Learn about our Solar System
+        </Link>
+        <Link to="/stage" className="stage-li">
+          Learn about NASA'S inventions
+        </Link>
+        <Link to="/apod" className="apod-li">
+          See interesting astronomy pictures
+        </Link>
+        <Link to="/about" className="about-li">
+          Learn more about the project
+        </Link>
       </div>
     </div>
   )
