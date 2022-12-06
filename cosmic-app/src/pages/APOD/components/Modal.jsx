784a8a0b00079f491c8db284ef0b300dad6d2c8c
bmikolajczak <br.mikolajczak@gmail.com>
2022-12-06 01:10:59 +0100

    redux state i setup apod modala

---
+++ cosmic-app/src/pages/APOD/components/Modal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export function ApodModal() {
+  return (
+    <div>
+      <button>Close modal</button>
+      <div>
+        <div>
+          <img />
+        </div>
+        <div>
+          <h1>Picture title </h1>
+          <p>Picture description</p>
+          <button>Save to Your Gallery</button>
+        </div>
+      </div>
+    </div>
+  )
+}
