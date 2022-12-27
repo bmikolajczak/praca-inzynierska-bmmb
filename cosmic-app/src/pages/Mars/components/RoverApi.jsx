e42919508cad4680ca0cf3ba2e7c53ec0459c9a7
M4thez <namemat@gmail.com>
2022-12-27 16:27:11 +0100

    WiP rover photos api

--- cosmic-app/src/pages/Mars/components/RoverApi.jsx
+++ cosmic-app/src/pages/Mars/components/RoverApi.jsx
@@ -1,13 +1,31 @@
 import style from '../styles/RoverApi.module.scss'
-import React from 'react'
-import FetchMarsApi from './FetchMarsApi'
+import React, { useState, useEffect } from 'react'
+import SelectForm from './SelectForm'
 
 export default function RoverApi() {
+  const [loaded, setLoaded] = useState(false)
+  const [data, setData] = useState([])
+  const baseUrl = 'https://api.nasa.gov/mars-photos/api/v1'
+  const apiKey = '0381f1py7G8yhbs9VvrxN9JPn2O5LJ88EEqolGND'
+  // run on start
+  useEffect(() => {
+    fetch(baseUrl + '/rovers?api_key=' + apiKey)
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result)
+        setLoaded(true)
+      })
+  }, [])
+
   return (
     <section className={style.roverApi}>
       <h2>Mars Rover Photos</h2>
-      <div>
-        <FetchMarsApi apiKey='0381f1py7G8yhbs9VvrxN9JPn2O5LJ88EEqolGND'/>
+      <div className={style.formsContainer}>
+        {loaded ? 
+          <SelectForm name="rovers" labelText="Available rovers " data={data.rovers} /> 
+          : <div>Loading...</div>
+        }
+        
       </div>
     </section>
   )
---
+++ cosmic-app/src/pages/Mars/components/SelectForm.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+
+export default function SelectForm(props) {
+
+  const [selectedItem, setItem] = useState('empty')
+  const [data, setData] = useState([])
+  const [loaded, setLoaded]= useState(false)
+
+  function handleChange(event) {
+    setItem(event.target.value)
+    console.log(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(selectedItem)
+    fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/'+ selectedItem +'/latest_photos?api_key=' + '0381f1py7G8yhbs9VvrxN9JPn2O5LJ88EEqolGND')
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result.latest_photos)
+        setLoaded(true)
+        console.log(data);
+      })
+  }
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor={props.name}>{props.labelText}</label>
+          <select name={props.name} id={props.name} onChange={handleChange}>
+            <option value='empty'></option>
+            {props.data.map(item => (
+              <option key={item.name} value={item.name}>
+                {item.name}
+              </option>
+            ))}
+          </select>
+          <input type="submit" value="Submit"/>
+        </form>
+        {loaded && data.length &&
+          <div>
+            <p>{data[0].img_src}</p>
+            <img src={data[0].img_src}/>
+          </div>
+        }
+      </div>
+    )
+}
