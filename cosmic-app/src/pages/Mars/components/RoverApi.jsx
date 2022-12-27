d8b1045e0572619ee9336a89813885b7be636dd5
M4thez <namemat@gmail.com>
2022-12-27 14:24:30 +0100

    Basic fetch

--- cosmic-app/src/pages/Mars/components/RoverApi.jsx
+++ cosmic-app/src/pages/Mars/components/RoverApi.jsx
@@ -1,10 +1,14 @@
 import style from '../styles/RoverApi.module.scss'
-import React, { Suspense, useRef, useState } from 'react'
+import React from 'react'
+import FetchMarsApi from './FetchMarsApi'
 
 export default function RoverApi() {
   return (
     <section className={style.roverApi}>
       <h2>Mars Rover Photos</h2>
+      <div>
+        <FetchMarsApi apiKey='0381f1py7G8yhbs9VvrxN9JPn2O5LJ88EEqolGND'/>
+      </div>
     </section>
   )
 }
