e717405053c2a3d1710ec46316829fef03e5e621
M4thez <namemat@gmail.com>
2022-12-27 13:28:07 +0100

    Rover api kickstart

---
+++ cosmic-app/src/pages/Mars/components/RoverApi.jsx
@@ -0,0 +1,10 @@
+import style from '../styles/RoverApi.module.scss'
+import React, { Suspense, useRef, useState } from 'react'
+
+export default function RoverApi() {
+  return (
+    <section className={style.roverApi}>
+      <h2>Mars Rover Photos</h2>
+    </section>
+  )
+}
