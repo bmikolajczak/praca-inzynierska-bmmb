0b5f521594394a455469872e7cf5bb8738cfef79
M4thez <namemat@gmail.com>
2023-01-05 15:11:39 +0100

    Made loader bigger

--- cosmic-app/src/infrastructure/loader/LoaderCustom.jsx
+++ cosmic-app/src/infrastructure/loader/LoaderCustom.jsx
@@ -4,8 +4,9 @@ export default function LoaderCustom() {
   return (
     <Loader
     containerStyles={{'background': '#040404'}}
-    innerStyles={{'height': '10px', 'borderRadius': '8px'}}
-    barStyles={{'height': '10px', 'borderRadius': '8px'}} 
+    innerStyles={{'height': '12px', 'borderRadius': '8px'}}
+    barStyles={{'height': '12px', 'borderRadius': '8px'}}
+    dataStyles={{'fontSize': '12px'}}
     dataInterpolation={(p) => `Loading ${p.toFixed(2)}%`}
     />
   )
