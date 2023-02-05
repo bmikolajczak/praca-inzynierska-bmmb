603b786f14b6b65a2ad774f4f15ca6d2a77ed92f
bmikolajczak <br.mikolajczak@gmail.com>
2023-02-05 21:59:03 +0100

    fixed err with saved imgs in profile

--- cosmic-app/src/pages/Account/components/Account.jsx
+++ cosmic-app/src/pages/Account/components/Account.jsx
@@ -19,9 +19,6 @@ import { changeUserLoggedIn, showChosenPhoto, setChosenPhoto } from '../../../in
 import { BsFillTrashFill } from 'react-icons/bs'
 
 export function Account() {
-  const [userEmail, setUserEmail] = useState('')
-  const [userPassword, setUserPassword] = useState('')
-
   //Saved images
   const [savedImages, setSavedImages] = useState([])
 
@@ -40,7 +37,11 @@ export function Account() {
   async function GetSavedImages() {
     const userDocSnapshot = await getDoc(userDocRef)
     if (userDocSnapshot.exists()) {
-      setSavedImages(userDocSnapshot.data().savedImages)
+      if (userDocSnapshot.data().savedImages) {
+        setSavedImages(userDocSnapshot.data().savedImages)
+      } else {
+        return
+      }
     }
   }
   //CHECKING WETHER USER IS LOGGED IN
@@ -105,91 +106,31 @@ export function Account() {
       {userLoggedIn ? (
         <div className={styles['saved-images']}>
           <p className={styles['images-header']}>Saved Images</p>
-          <div className={styles['cards']}>
-            {savedImages.map((image, index) => (
-              <div key={image.title} className={styles['image-card']}>
-                <div className={styles['card-visuals']}>
-                  <img
-                    onClick={() => updateChosenPic(image)}
-                    src={image.url}
-                    alt={image.title}
-                    className={styles['fetched-photo']}
-                  />
-                  <p className={styles['image-title']}>{image.title}</p>
-                  <button id={styles['remove-btn']} onClick={() => removeImage(image)}>
-                    <BsFillTrashFill />
-                  </button>
+          {savedImages.length !== 0 ? (
+            <div className={styles['cards']}>
+              {savedImages.map((image, index) => (
+                <div key={image.title} className={styles['image-card']}>
+                  <div className={styles['card-visuals']}>
+                    <img
+                      onClick={() => updateChosenPic(image)}
+                      src={image.url}
+                      alt={image.title}
+                      className={styles['fetched-photo']}
+                    />
+                    <p className={styles['image-title']}>{image.title}</p>
+                    <button id={styles['remove-btn']} onClick={() => removeImage(image)}>
+                      <BsFillTrashFill />
+                    </button>
+                  </div>
+                  <p className={styles['image-desc']}>{image.explanation}</p>
                 </div>
-                <p className={styles['image-desc']}>{image.explanation}</p>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          ) : (
+            <h2>You didn't save any images yet</h2>
+          )}
         </div>
       ) : null}
     </div>
   )
 }
-
-// async function loginUser() {
-//   try {
-//     const user = await signInWithEmailAndPassword(
-//       auth,
-//       loginEmail,
-//       loginPassword
-//     )
-//     console.log('user: ', user.user)
-//   } catch (error) {
-//     console.log(error.message)
-//   }
-// }
-
-// async function signInGoogle() {
-//   try {
-//     const result = await signInWithPopup(auth, googleProvider)
-//     const credential = GoogleAuthProvider.credentialFromResult(result)
-//     const token = credential.accessToken
-//     //info about signd in user
-//     const user = result.user
-//     const newUserRef = await setDoc(doc(usersRef, user.uid), {
-//       name: user.displayName,
-//       email: user.email,
-//     })
-//     console.log(newUserRef)
-//   } catch (error) {
-//     const errorCode = error.code
-//     const errorMessage = error.message
-//     // The email of the user's account used.
-//     const email = error.customData.email
-//     // The AuthCredential type that was used.
-//     const credential = GoogleAuthProvider.credentialFromError(error)
-//     console.log(
-//       'code: ',
-//       errorCode,
-//       'message: ',
-//       errorMessage,
-//       'email: ',
-//       email,
-//       'credential used: ',
-//       credential
-//     )
-//   }
-// }
-
-//async function registerUser() {
-//   try {
-//     const user = await createUserWithEmailAndPassword(auth, userEmail, userPassword)
-//     console.log(user.user.uid)
-//     try {
-//       const newUserRef = await setDoc(doc(usersRef, user.user.uid), {
-//         name: 'Testy',
-//         surname: 'Smith',
-//         email: userEmail,
-//       })
-//       console.log('New document with user info: ', newUserRef)
-//     } catch (error) {
-//       console.log('Problem when creating user:', error)
-//     }
-//   } catch (error) {
-//     console.log('OOh no,', error.message)
-//   }
-// }
